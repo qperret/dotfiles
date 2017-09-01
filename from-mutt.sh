9009152f616211697753aa525bbacbce8d397bbe
Quentin Perret <quentin.perret@arm.com>
2017-09-01 17:25:38 +0100

    mutt: add from-mutt.sh to save incoming patches on local drive

---
+++ from-mutt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to save patches from lists
+
+PATCH_FILE=`mktemp --tmpdir="/data/tmp"  mutt-patch.XXXXXX`
+cat > $PATCH_FILE
+MAILBOX=`cat $PATCH_FILE | formail -c -xSubject: | tr "'" "." | sed -e '{ s@\[@@g; s@\]@@g; s@[*()" \t]@_@g; s@[/:]@-@g; s@^ \+@@; s@\.\.@.@g; s@-_@_@g; s@__@_@g; s@\.$@@; }' | cut -c 1-70`.patch
+
+mv "$PATCH_FILE" "/data/work/incoming/$MAILBOX"

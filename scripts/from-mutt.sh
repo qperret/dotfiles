07c023a56b2f08400b12f94fd8e5091312e82609
Quentin Perret <quentin.perret@arm.com>
2018-01-03 18:20:35 +0000

    from-mutt: relocate inbox folder

--- scripts/from-mutt.sh
+++ scripts/from-mutt.sh
@@ -5,4 +5,4 @@ PATCH_FILE=`mktemp --tmpdir="/data/tmp"  mutt-patch.XXXXXX`
 cat > $PATCH_FILE
 MAILBOX=`cat $PATCH_FILE | formail -c -xSubject: | tr "'" "." | sed -e '{ s@\[@@g; s@\]@@g; s@[*()" \t]@_@g; s@[/:]@-@g; s@^ \+@@; s@\.\.@.@g; s@-_@_@g; s@__@_@g; s@\.$@@; }' | cut -c 1-70`.patch
 
-mv "$PATCH_FILE" "/data/work/incoming/$MAILBOX"
+mv "$PATCH_FILE" "/data/misc/patches/incoming/$MAILBOX"
